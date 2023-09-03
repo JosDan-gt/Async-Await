41acd41524bfe80c49d7777154adbe37ec1241ea
JosDan-gt <jaldanaa3@miumg.edu.gt>
2023-09-03 17:11:57 -0600

    Responde

--- JS/javaSC.js
+++ JS/javaSC.js
@@ -1,17 +1,35 @@
+window.onload = function () {
+    Llamada1();
+}
 
-window.addEventListener('load', function () {
-    Llamada1()
-});
+const selectUbication = document.getElementById("selectUbication")
+selectUbication.addEventListener("change", function () {
+    Llamada1();
+})
 
 function Llamada1() {
-    let xhr = new XMLHttpRequest;
-    xhr.open('GET', 'https://censopoblacion.gt/indicadores/1/999');
-    xhr.send();
-    xhr.addEventListener('load', function () {
-        let result = JSON.parse(xhr.responseText);
-        for (let i of result) {
-            console.log(i)
-        }
-    });
+    var selectUbication = document.getElementById("selectUbication");
 
+    var url = "https://censopoblacion.gt/indicadores/1/999"; // URL predeterminada
+
+    if (selectUbication !== null) {
+        var selectedValue = selectUbication.value;
+        if (selectedValue !== "Selecciona un Departamento") { // Verificar que no sea el valor predeterminado
+            url = "https://censopoblacion.gt/indicadores/" + selectedValue + "/999";
+        }
+    }
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error => {
+            console.error('Error al obtener datos:', error);
+        });
 }

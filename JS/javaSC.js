9201f233423aacbe68bfc54c454f722b13879105
JosDan-gt <jaldanaa3@miumg.edu.gt>
2023-09-03 11:54:29 -0600

    Arreglo

--- JS/javaSC.js
+++ JS/javaSC.js
@@ -1,6 +1,6 @@
 
 window.addEventListener('load', function () {
-    Llamada1();
+    Llamada1()
 });
 
 function Llamada1() {
@@ -8,13 +8,9 @@ function Llamada1() {
     xhr.open('GET', 'https://censopoblacion.gt/indicadores/1/999');
     xhr.send();
     xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-            let result = JSON.parse(xhr.responseText);
-            for (let i of result) {
-                console.log(i.nombre);
-            }
-        } else {
-            console.error('Error al cargar datos:', xhr.status);
+        let result = JSON.parse(xhr.responseText);
+        for (let i of result) {
+            console.log(i)
         }
     });
 
